ceae43a6d3ad5d651b136aa447d7a4b768f9a1aa
Fred Morrison <fpmorrison@verizon.net>
2018-08-21 21:53:42 -0400

    Add Vehicle abstraction

---
+++ Vehicle.java
@@ -0,0 +1,17 @@
+
+public abstract class Vehicle implements Moveable {
+	private int maximumPassengers;
+	
+	@Override
+	public String move() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public int getMaximumPassengers() {
+		return maximumPassengers;
+	}
+	public void setMaximumPassengers(int maximumPassengers) {
+		this.maximumPassengers = maximumPassengers;
+	}
+
+}
